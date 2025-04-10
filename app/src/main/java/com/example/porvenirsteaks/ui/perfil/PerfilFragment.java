e79703a9f8e074102ecca157d2b08260a693213a
Christian Vasquez <VasqCD@users.noreply.github.com>
2025-04-09 21:28:23 -0600

    Add PerfilFragment for user profile management UI

---
+++ app/src/main/java/com/example/porvenirsteaks/ui/perfil/PerfilFragment.java
@@ -0,0 +1,27 @@
+package com.example.porvenirsteaks.ui.perfil;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.example.porvenirsteaks.databinding.FragmentPerfilBinding;
+
+public class PerfilFragment extends Fragment {
+    private FragmentPerfilBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentPerfilBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
