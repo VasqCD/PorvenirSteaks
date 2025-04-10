512dd09b7f352f56564e29ee38c8c6c81334bd83
Christian Vasquez <VasqCD@users.noreply.github.com>
2025-04-09 23:10:37 -0600

    Add image compression and URI path retrieval methods in ImageUtils

--- app/src/main/java/com/example/porvenirsteaks/utils/ImageUtils.java
+++ app/src/main/java/com/example/porvenirsteaks/utils/ImageUtils.java
@@ -1,10 +1,23 @@
 package com.example.porvenirsteaks.utils;
 
+import android.content.ContentUris;
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.bumptech.glide.request.RequestOptions;
 import com.example.porvenirsteaks.R;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class ImageUtils {
     public static void loadImage(ImageView imageView, String imageUrl) {
@@ -46,5 +59,127 @@ public class ImageUtils {
         } else {
             imageView.setImageResource(R.drawable.user_placeholder);
         }
+    }
+
+    // Métodos adicionales para tu clase ImageUtils.java
+
+    /**
+     * Comprime una imagen para reducir su tamaño antes de subirla
+     */
+    public static File compressImage(Context context, Uri imageUri) {
+        try {
+            // Obtener bitmap de la Uri
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), imageUri);
+
+            // Comprimir a una calidad media (70%)
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 70, baos);
+
+            // Crear archivo temporal para guardar la imagen comprimida
+            File outputDir = context.getCacheDir();
+            File outputFile = File.createTempFile("compressed_", ".jpg", outputDir);
+
+            // Escribir bytes comprimidos al archivo
+            FileOutputStream fos = new FileOutputStream(outputFile);
+            fos.write(baos.toByteArray());
+            fos.close();
+
+            return outputFile;
+        } catch (Exception e) {
+            Log.e("ImageUtils", "Error al comprimir imagen", e);
+            return null;
+        }
+    }
+
+    /**
+     * Obtiene el path real de un Uri
+     */
+    public static String getRealPathFromURI(Context context, Uri uri) {
+        // Para documentos de tipo "content://"
+        if (DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                String docId = DocumentsContract.getDocumentId(uri);
+                String[] split = docId.split(":");
+                String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                String docId = DocumentsContract.getDocumentId(uri);
+                String[] split = docId.split(":");
+                String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                String selection = "_id=?";
+                String[] selectionArgs = new String[]{split[1]};
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+                String id = DocumentsContract.getDocumentId(uri);
+                if (id.startsWith("raw:")) {
+                    return id.substring(4);
+                }
+
+                Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                return getDataColumn(context, contentUri, null, null);
+            }
+        }
+        // MediaStore (y otros content providers)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    private static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {column};
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+        return null;
+    }
+
+    private static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    private static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    private static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
     }
 }
