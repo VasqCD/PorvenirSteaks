623976d9fed5337045b2a929e4b64fd154217e5e
Christian Vasquez <VasqCD@users.noreply.github.com>
2025-04-10 11:15:42 -0600

    Add UserResponse model to encapsulate user data in API responses

---
+++ app/src/main/java/com/example/porvenirsteaks/data/model/responses/UserResponse.java
@@ -0,0 +1,15 @@
+package com.example.porvenirsteaks.data.model.responses;
+
+import com.example.porvenirsteaks.data.model.User;
+
+public class UserResponse {
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
